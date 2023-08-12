eb2d7bb0a284472cbb985f2e12862dc1f3b67119
ayacouba <96453820+ayacouba@users.noreply.github.com>
2023-08-12 15:42:17 -0400

    Updated next.config.js

--- next.config.js
+++ next.config.js
@@ -1,4 +1,7 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {};
 
-module.exports = nextConfig
+module.exports = {
+  basePath: "/aimade-portfolio1",
+  assetPrefix: "/aimade-portfolio1/",
+};
